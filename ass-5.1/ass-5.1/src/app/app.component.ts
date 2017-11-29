bb0ee714cf97823e3ad8c045d7c8e80df045d2d8
jasmitha <jasmieyjas@gmail.com>
2017-11-29 21:39:22 +0530

    Add files via upload

---
+++ ass-5.1/ass-5.1/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+//defining the component selector ,template and styleUrls
+@Component({
+  selector: 'app-component',
+  template: ``,
+  styleUrls: ['./app.component.css']
+})
+export class RootComponent {
+}
